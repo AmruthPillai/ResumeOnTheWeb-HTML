b03e9a043b85803244543653f024f7c3f5ce2f01
AmruthPillai <im.amruth@gmail.com>
2017-06-11 22:19:00 +0530

    great progress, reaching the end

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,3 @@
-$('#scroll-more').click(function() {
-  $('html, body').animate({
-    scrollTop: $('#scroll-hook').offset().top
-  }, 'slow');
-});
-
 const photos = ['IMG_3522', 'IMG_1880', 'IMG_8295', 'IMG_5184', 'IMG_8286', 'IMG_4861'];
 
 $(document).ready(function() {
@@ -15,3 +9,25 @@ $(document).ready(function() {
     if (i >= photos.length) i = 0;
   }, 5000);
 });
+
+$('#scroll-to-content').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#scroll-hook').offset().top
+  }, 'slow');
+});
+
+$('#scroll-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 'slow');
+});
+
+var resumeSwitch = document.querySelector('#resume-switch');
+var resumeSwitchInit = new Switchery(resumeSwitch, { color: '#444' });
+resumeSwitch.onchange = function() {
+  if (resumeSwitch.checked) {
+    $('.resume-section').show();
+  } else {
+    $('.resume-section').hide();
+  }
+}
