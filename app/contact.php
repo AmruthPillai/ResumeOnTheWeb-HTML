4296fabcd475761d1dfa265e5560ede2bf0f85e3
AmruthPillai <im.amruth@gmail.com>
2017-06-16 12:17:12 +0530

    it's finally working, just editing the format a bit

--- app/contact.php
+++ app/contact.php
@@ -1,10 +1,13 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
-  if ( (isset($_POST['name'])) && (isset($_POST['email']) && $_POST['message']!="") ) {
+  header('Access-Control-Allow-Origin: *');
+  if ( (isset($_POST['name'])) && (isset($_POST['email']) && $_POST['message']!='') ) {
     $to = 'im.amruth@gmail.com';
     $subject = 'Someone has sent you a message on amruthpillai.com!';
     $message = '
       <html>
+        <head>
+          <title>Message from Amruth Pillai\'s Resume on the Web</title>
+        </head>
         <body>
           <p><b>Name:</b> '.$_POST['name'].'</p>
           <p><b>Email:</b> '.$_POST['email'].'</p>
@@ -12,9 +15,11 @@
         </body>
       </html>
     ';
-    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-    $headers .= "From: Site <contact@amruthpillai.com>\r\n";
+    $headers  = 'Content-type: text/html; charset=utf-8 \r\n';
+    $headers .= 'From: '.$_POST['name'].'<'.$_POST['email'].'>\r\n';
+
     mail($to, $subject, $message, $headers);
+
     echo json_encode(array('status' => 'success'));
   } else {
     echo json_encode(array('status' => 'error'));
